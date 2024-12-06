8e49e99a45cb4429a63bf6ad7a69290158220654
clavaire2 <clavaire.gomina@gmail.com>
2024-12-06 19:01:25 +0000

    s

--- cadastre (2).sql
+++ cadastre (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 05 déc. 2024 à 22:04
+-- Généré le : ven. 06 déc. 2024 à 20:00
 -- Version du serveur : 10.6.9-MariaDB
 -- Version de PHP : 8.1.25
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ident` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email_admin` varchar(255) NOT NULL,
+  `numero_telephone` varchar(15) DEFAULT NULL,
+  `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`ident`, `name`, `prenom`, `email_admin`, `numero_telephone`, `date_inscription`, `password`) VALUES
+(1, 'Abalo cb', 'cf', 'abalo@gmail.com', '66565', '2024-12-06 18:55:54', 'ea66e9c169c7e5a307eb06fb8e6a239d');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `brigade`
 --
 
@@ -34,16 +57,17 @@ CREATE TABLE `brigade` (
   `email_brigade` varchar(255) NOT NULL,
   `numero_telephone` varchar(15) DEFAULT NULL,
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `brigade`
 --
 
-INSERT INTO `brigade` (`ident`, `name`, `prenom`, `email_brigade`, `numero_telephone`, `date_inscription`, `password`) VALUES
-(1, 'c Gomina clavaire', 'cc', 'clavaire.gominab@gmail.com', '92891445', '2024-12-04 10:59:12', '5669fb4cc8a47469cc15eb3e24dc8f23'),
-(2, 'c Gomina clavaire', 'cc', 'clavaire.gominabb@gmail.com', '9289144', '2024-12-04 22:11:35', '5669fb4cc8a47469cc15eb3e24dc8f23');
+INSERT INTO `brigade` (`ident`, `name`, `prenom`, `email_brigade`, `numero_telephone`, `date_inscription`, `password`, `reset_token`, `token_expiration`) VALUES
+(1, 'Abalo ', 'b', 'abalo_b@gmail.com', '66565', '2024-12-06 18:28:10', 'ea66e9c169c7e5a307eb06fb8e6a239d', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -58,16 +82,17 @@ CREATE TABLE `chef_brigade` (
   `email_chefbrigade` varchar(255) NOT NULL,
   `numero_telephone` varchar(15) DEFAULT NULL,
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `chef_brigade`
 --
 
-INSERT INTO `chef_brigade` (`ident`, `name`, `prenom`, `email_chefbrigade`, `numero_telephone`, `date_inscription`, `password`) VALUES
-(1, 'c Gomina clavaire', 'cc', 'clavaire.gomina@gmail.com', '92891445', '2024-12-04 02:10:31', '5669fb4cc8a47469cc15eb3e24dc8f23'),
-(3, 'c Gomina clavaire', 'cc', 'clavaire.gominaqq@gmail.com', '9289144', '2024-12-04 19:48:31', '5669fb4cc8a47469cc15eb3e24dc8f23');
+INSERT INTO `chef_brigade` (`ident`, `name`, `prenom`, `email_chefbrigade`, `numero_telephone`, `date_inscription`, `password`, `reset_token`, `token_expiration`) VALUES
+(4, 'Abalo cb', 'cb', 'abalo_cb@gmail.com', '66565', '2024-12-06 18:19:59', 'ea66e9c169c7e5a307eb06fb8e6a239d', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -82,15 +107,17 @@ CREATE TABLE `conversation_fonciere` (
   `email_conversationfonciere` varchar(255) NOT NULL,
   `numero_telephone` varchar(15) DEFAULT NULL,
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `conversation_fonciere`
 --
 
-INSERT INTO `conversation_fonciere` (`ident`, `name`, `prenom`, `email_conversationfonciere`, `numero_telephone`, `date_inscription`, `password`) VALUES
-(2, 'c Gomina clavaire', 'cc', 'clavaire.gominacf@gmail.com', '9289144', '2024-12-05 18:02:29', '5669fb4cc8a47469cc15eb3e24dc8f23');
+INSERT INTO `conversation_fonciere` (`ident`, `name`, `prenom`, `email_conversationfonciere`, `numero_telephone`, `date_inscription`, `password`, `reset_token`, `token_expiration`) VALUES
+(1, 'Abalo cb', 'cf', 'abalo_cf@gmail.com', '66565', '2024-12-06 18:48:30', 'ea66e9c169c7e5a307eb06fb8e6a239d', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -120,15 +147,17 @@ CREATE TABLE `evaluation_cadastrale` (
   `email_evaluationcadastrale` varchar(255) NOT NULL,
   `numero_telephone` varchar(15) DEFAULT NULL,
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `evaluation_cadastrale`
 --
 
-INSERT INTO `evaluation_cadastrale` (`ident`, `name`, `prenom`, `email_evaluationcadastrale`, `numero_telephone`, `date_inscription`, `password`) VALUES
-(1, 'c Gomina clavaire', 'cc', 'clavaire.gominaee@gmail.com', '9289144', '2024-12-05 12:17:11', '5669fb4cc8a47469cc15eb3e24dc8f23');
+INSERT INTO `evaluation_cadastrale` (`ident`, `name`, `prenom`, `email_evaluationcadastrale`, `numero_telephone`, `date_inscription`, `password`, `reset_token`, `token_expiration`) VALUES
+(1, 'Abalo ', 'cb', 'abalo_ec@gmail.com', '66565', '2024-12-06 18:46:59', 'ea66e9c169c7e5a307eb06fb8e6a239d', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -147,20 +176,6 @@ CREATE TABLE `gestion_brigade` (
   `validateur` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `gestion_brigade`
---
-
-INSERT INTO `gestion_brigade` (`id`, `nom_dossier`, `date_creation`, `statut`, `raison`, `chef_brigade_id`, `brigade_id`, `validateur`) VALUES
-(1, 'zz', '2024-12-04 20:03:18', 'En cours', NULL, 1, NULL, 'c Gomina clavaire cc'),
-(2, 'azxz', '2024-12-04 20:28:58', 'En cours', NULL, 1, NULL, 'c Gomina clavaire cc'),
-(3, 'azxz', '2024-12-04 20:36:50', 'Terminé', NULL, 1, 2, 'c Gomina clavaire cc'),
-(4, 'lk', '2024-12-05 00:32:38', 'Terminé', NULL, 1, 2, 'c Gomina clavaire cc'),
-(5, 'hoooo', '2024-12-05 09:16:48', 'Terminé', NULL, 1, 1, 'c Gomina clavaire cc'),
-(6, 'zrzf', '2024-12-05 09:48:24', 'Terminé', NULL, 1, 2, 'c Gomina clavaire cc'),
-(7, 'zzfzef', '2024-12-05 09:50:17', 'Terminé', NULL, 1, 2, 'c Gomina clavaire cc'),
-(8, 'ssaa', '2024-12-05 10:38:52', 'Terminé', NULL, 3, 2, 'c Gomina clavaire cc');
-
 -- --------------------------------------------------------
 
 --
@@ -178,14 +193,6 @@ CREATE TABLE `gestion_chef_brigade` (
   `chef_assigned` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `gestion_chef_brigade`
---
-
-INSERT INTO `gestion_chef_brigade` (`id`, `nom_dossier`, `date_creation`, `date_envoi`, `statut`, `raison`, `chef_brigade_id`, `chef_assigned`) VALUES
-(7, 'zrzf', '2024-12-05 09:38:55', '2024-12-05 09:48:24', 'Terminé', NULL, 1, NULL),
-(8, 'zzfzef', '2024-12-05 09:49:35', '2024-12-05 09:50:17', 'Terminé', NULL, 1, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -201,17 +208,6 @@ CREATE TABLE `gestion_evaluation_cadastrale` (
   `statut` enum('En cours','Terminé','En attente') DEFAULT 'En cours',
   `evaluation_cadastrale_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `gestion_evaluation_cadastrale`
---
-
-INSERT INTO `gestion_evaluation_cadastrale` (`id`, `nom_dossier`, `date_creation`, `date_validation`, `nom_evaluation_cadastrale`, `statut`, `evaluation_cadastrale_id`) VALUES
-(1, '4', '0000-00-00 00:00:00', '2024-12-05 11:18:12', 'c Gomina clavaire cc', 'Terminé', 1),
-(2, '5', '0000-00-00 00:00:00', '2024-12-05 11:37:35', 'c Gomina clavaire cc', 'Terminé', 1),
-(3, 'ssaa', '2024-12-05 10:38:52', '2024-12-05 11:46:27', 'c Gomina clavaire cc', 'Terminé', 1),
-(4, '4', NULL, '2024-12-05 13:45:07', 'c Gomina clavaire cc', 'En attente', NULL),
-(5, '4', NULL, '2024-12-05 13:45:37', 'c Gomina clavaire cc', 'En attente', NULL);
 
 -- --------------------------------------------------------
 
@@ -230,14 +226,6 @@ CREATE TABLE `gestion_fonciere` (
   `validateur` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `gestion_fonciere`
---
-
-INSERT INTO `gestion_fonciere` (`id`, `nom_dossier`, `date_creation`, `date_validation`, `nom_signature`, `statut`, `fonciere_id`, `validateur`) VALUES
-(1, 'ssaa', '2024-12-05 10:38:52', '2024-12-05 17:32:22', 'c Gomina clavaire cc', 'Terminé', 2, NULL),
-(2, '5', NULL, '2024-12-05 17:53:08', 'c Gomina clavaire cc', 'Terminé', 2, 'c Gomina clavaire cc');
-
 -- --------------------------------------------------------
 
 --
@@ -253,17 +241,6 @@ CREATE TABLE `gestion_securisation` (
   `statut` enum('En cours','Terminé','En attente') DEFAULT 'En cours',
   `securisation_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `gestion_securisation`
---
-
-INSERT INTO `gestion_securisation` (`id`, `nom_dossier`, `date_creation`, `date_validation`, `nom_brigade`, `statut`, `securisation_id`) VALUES
-(1, 'azxz', '2024-12-04 20:36:50', '2024-12-05 00:23:29', 'c Gomina clavaire cc', '', NULL),
-(2, 'lk', '2024-12-05 00:32:38', '2024-12-05 00:35:13', 'c Gomina clavaire cc', 'Terminé', 1),
-(4, 'zrzf', '2024-12-05 09:48:24', '2024-12-05 10:45:58', 'c Gomina clavaire cc', 'Terminé', 1),
-(5, 'zzfzef', '2024-12-05 09:50:17', '2024-12-05 10:45:59', 'c Gomina clavaire cc', 'Terminé', 1),
-(6, 'ssaa', '2024-12-05 10:38:52', '2024-12-05 10:45:59', 'c Gomina clavaire cc', 'Terminé', 1);
 
 -- --------------------------------------------------------
 
@@ -281,14 +258,6 @@ CREATE TABLE `gestion_signature` (
   `signature_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `gestion_signature`
---
-
-INSERT INTO `gestion_signature` (`id`, `nom_dossier`, `date_creation`, `date_validation`, `nom_evaluation_cadastrale`, `statut`, `signature_id`) VALUES
-(1, 'ssaa', '2024-12-05 10:38:52', '2024-12-05 13:47:55', 'c Gomina clavaire cc', 'Terminé', 1),
-(2, '5', NULL, '2024-12-05 13:51:02', 'c Gomina clavaire cc', 'Terminé', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -302,16 +271,17 @@ CREATE TABLE `securisation` (
   `email_securisation` varchar(255) NOT NULL,
   `numero_telephone` varchar(15) DEFAULT NULL,
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `securisation`
 --
 
-INSERT INTO `securisation` (`ident`, `name`, `prenom`, `email_securisation`, `numero_telephone`, `date_inscription`, `password`) VALUES
-(1, 'c Gomina clavaire', 'cc', 'clavaire.gominass@gmail.com', '9289144', '2024-12-05 01:07:38', '5669fb4cc8a47469cc15eb3e24dc8f23'),
-(2, 'aed', 'e', 'sec@gmail.com', '24', '2024-12-05 02:07:28', '5669fb4cc8a47469cc15eb3e24dc8f23');
+INSERT INTO `securisation` (`ident`, `name`, `prenom`, `email_securisation`, `numero_telephone`, `date_inscription`, `password`, `reset_token`, `token_expiration`) VALUES
+(1, 'Abalo ', 'cb', 'abalo_s@gmail.com', '66565', '2024-12-06 18:45:52', 'ea66e9c169c7e5a307eb06fb8e6a239d', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -326,19 +296,29 @@ CREATE TABLE `signature` (
   `email_signature` varchar(255) NOT NULL,
   `numero_telephone` varchar(15) DEFAULT NULL,
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `signature`
 --
 
-INSERT INTO `signature` (`ident`, `name`, `prenom`, `email_signature`, `numero_telephone`, `date_inscription`, `password`) VALUES
-(1, 'c Gomina clavaire', 'cc', 'clavaire.gominas@gmail.com', '9289144', '2024-12-05 14:31:21', '5669fb4cc8a47469cc15eb3e24dc8f23');
+INSERT INTO `signature` (`ident`, `name`, `prenom`, `email_signature`, `numero_telephone`, `date_inscription`, `password`, `reset_token`, `token_expiration`) VALUES
+(1, 'Abalo cb', 's', 'abalo_s@gmail.com', '66565', '2024-12-06 18:47:41', 'ea66e9c169c7e5a307eb06fb8e6a239d', NULL, NULL);
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ident`),
+  ADD UNIQUE KEY `email_admin` (`email_admin`),
+  ADD UNIQUE KEY `numero_telephone` (`numero_telephone`);
 
 --
 -- Index pour la table `brigade`
@@ -437,16 +417,22 @@ ALTER TABLE `signature`
 --
 
 --
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT pour la table `brigade`
 --
 ALTER TABLE `brigade`
-  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `chef_brigade`
 --
 ALTER TABLE `chef_brigade`
-  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `conversation_fonciere`
@@ -458,7 +444,7 @@ ALTER TABLE `conversation_fonciere`
 -- AUTO_INCREMENT pour la table `dossier`
 --
 ALTER TABLE `dossier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `evaluation_cadastrale`
@@ -470,43 +456,43 @@ ALTER TABLE `evaluation_cadastrale`
 -- AUTO_INCREMENT pour la table `gestion_brigade`
 --
 ALTER TABLE `gestion_brigade`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `gestion_chef_brigade`
 --
 ALTER TABLE `gestion_chef_brigade`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `gestion_evaluation_cadastrale`
 --
 ALTER TABLE `gestion_evaluation_cadastrale`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `gestion_fonciere`
 --
 ALTER TABLE `gestion_fonciere`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `gestion_securisation`
 --
 ALTER TABLE `gestion_securisation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `gestion_signature`
 --
 ALTER TABLE `gestion_signature`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `securisation`
 --
 ALTER TABLE `securisation`
-  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ident` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `signature`
